f8012ac83a4209bed68df2a7b84e11a71e0bdd57
Chengcheng-Xiao <iconxicon@me.com>
2020-12-30 17:43:45 +0000

    improve installation process for macOS.

--- install/install-ubuntu-dependencies.sh
+++ install/install-ubuntu-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install python-dev g++ libx11-dev mesa-common-dev libglu1-mesa-dev python-opengl python-numpy python-glade2
+sudo apt-get install python-dev g++ libx11-dev mesa-common-dev libglu1-mesa-dev python-opengl python-numpy python-glade2 libfltk1.3-dev
 # For documentation:
 sudo apt-get install python-epydoc doxygen
 
